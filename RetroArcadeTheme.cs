dbb5a384218512858345f7112cfc96e9eadb3678
defnotchris1 <itzchwis@gmail.com>
2025-10-19 21:20:03 -0500

    Update RetroArcadeTheme.cs

--- RetroArcadeTheme.cs
+++ RetroArcadeTheme.cs
@@ -20,14 +20,17 @@ namespace NekoBeats
             double barWidth = screenWidth / barCount;
             double maxHeight = screenHeight * 0.9;
 
-            // Detect beat
-            bool hasBeat = frequencies.Length > 0 && frequencies[0] > 50;
-            double currentIntensity = frequencies[0] / 100.0;
+            // Get sensitivity from MainWindow
+            double sensitivity = MainWindow.AudioSensitivity;
+
+            // Detect beat - sensitivity affects threshold
+            bool hasBeat = frequencies.Length > 0 && frequencies[0] > (50 / sensitivity);
+            double currentIntensity = (frequencies[0] / 100.0) * sensitivity;
 
             if (hasBeat && (DateTime.Now - lastBeatTime).TotalMilliseconds > 200)
             {
                 lastBeatTime = DateTime.Now;
-                beatIntensity = 1.0;
+                beatIntensity = 1.0 * sensitivity; // Sensitivity affects beat strength
             }
             else
             {
@@ -41,7 +44,9 @@ namespace NekoBeats
                 // Different patterns for each bar
                 double pattern1 = (Math.Sin(DateTime.Now.Millisecond * 0.018 + i * 0.6) + 1) * 0.4;
                 double pattern2 = (Math.Cos(DateTime.Now.Millisecond * 0.022 + i * 0.9) + 1) * 0.3;
-                double height = ((pattern1 + pattern2) / 2 * overallIntensity) * maxHeight;
+                
+                // APPLY SENSITIVITY to bar height
+                double height = ((pattern1 + pattern2) / 2 * overallIntensity * sensitivity) * maxHeight;
 
                 if (overallIntensity > 0.1)
                 {
@@ -65,9 +70,17 @@ namespace NekoBeats
                 dc.DrawRectangle(brush, new Pen(Brushes.White, 1), new Rect(x, y, barWidth - 2, height));
             }
 
-            // Pulsing border based on beat
-            var borderPen = new Pen(new SolidColorBrush(Color.FromRgb((byte)(255 * beatIntensity), 255, 255)), 3);
+            // Pulsing border based on beat - sensitivity affects border too
+            var borderIntensity = beatIntensity * sensitivity;
+            var borderPen = new Pen(new SolidColorBrush(Color.FromRgb((byte)(255 * borderIntensity), 255, 255)), 3);
             dc.DrawRectangle(null, borderPen, new Rect(10, 10, screenWidth - 20, screenHeight - 20));
+
+            // Debug info
+            var debugText = new FormattedText($"Retro - Sens: {sensitivity:F1}",
+                System.Globalization.CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface("Arial"), 14, Brushes.White);
+            dc.DrawText(debugText, new Point(20, 40));
         }
     }
 }
